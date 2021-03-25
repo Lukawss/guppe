db547fcbe2b8b791117888353eb7bdcafc86a8d1
Lucas de souza queiroz <lucas_s.q@hotmail.com>
2021-03-24 23:12:39 -0300

    Exercicios

--- exercicios_secao4.py
+++ exercicios_secao4.py
@@ -1,7 +1,6 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+with open('novo.txt', 'r') as arquivo, open('novo1.txt','a') as arquivo1:
+    arquivo1.write('Teste')
+    arquivo1.write('\n')
+    for linha in arquivo.readlines():
+        arquivo1.write(int(linha[1]))
 
-print(lista)
-
-lista.reverse()
-
-print(lista)
